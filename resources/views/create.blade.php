2d61712f4e1fd4d17ff387ca8f149da11d44a73d
rafiud <rayhanrafiudd@gmail.com>
2019-04-17 23:57:00 +0700

    add style

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,10 +2,33 @@
 <html>
 <head>
   <title>Tutorial Membuat CRUD Pada Laravel</title>
+  <style type="text/css">
+    body {
+      margin-left: 100px;
+      background-color: #22DE78;
+    }
+
+    input {
+      padding-left: 20px;
+      border: 1px solid #f4f4f4;
+      border-radius: 4px;
+      width: 250px;
+      height: 50px;
+    }
+
+    span {
+      color: white;
+    }
+
+    h1 {
+      color: white;
+    }
+
+  </style>
 </head>
 <body>
 
-  <h3>Data Buku</h3>
+  <h1>Tambah Buku</h1>
 
   <a href="/pegawai"> Kembali</a>
   
@@ -14,18 +37,18 @@
 
   <form action="/books/create" method="post">
     {{ csrf_field() }}
-    Judul Buku
+    <span>Judul Buku</span><br><br>
     <input type="text" name="judul" required="required"> <br/>
-
-    Penerbit
+    <br>
+    <span>Penerbit</span><br><br>
     <input type="text" name="penerbit" required="required"> <br/>
-
-    Tahun Terbit 
+    <br>
+    <span>Tahun Terbit</span><br><br>
     <input type="text" name="tahun_terbit" required="required"> <br/>
-
-    Pengarang 
+    <br>
+    <span>Pengarang</span> <br><br>
     <input type="text" name="pengarang" required="required"> <br/>
-    <input type="submit" value="Simpan Data">
+    <input type="submit" value="Simpan Data" style="margin-top: 30px;width:280px;">
   </form>
 
 </body>
